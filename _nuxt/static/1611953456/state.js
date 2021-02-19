69766010e324f6c7f5bb73d10e5bb25220070bd6
ThornWalli <ThornWalli@users.noreply.github.com>
2021-02-19 18:20:22 +0000

    deploy: 6e3f4e0eb7402c08d266d72060bf841fdfca1002

--- _nuxt/static/1611953456/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt-font-loader-strategy\u002F_nuxt\u002Fstatic\u002F1611953456",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{app:{basePath:"\u002Fnuxt-font-loader-strategy\u002F",assetsPath:"\u002Fnuxt-font-loader-strategy\u002F_nuxt\u002F",cdnURL:a}}}}(null));
